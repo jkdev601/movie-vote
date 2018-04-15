95a75b495f9e6c088c94d1ee847644e321b117cb
jkdev601 <januszkornaga@gmail.com>
2018-04-15 20:18:00 +0200

    change in index.js, new handler path

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
+import handleActions from './store/handler/handler';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
-import movieSelector from './store/reducer/movieSelector';
 import App from './App';
 import './style.css';
 
-const store = createStore(movieSelector, applyMiddleware(thunk));
+const store = createStore(handleActions, applyMiddleware(thunk));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
-
